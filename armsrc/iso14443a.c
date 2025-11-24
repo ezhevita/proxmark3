16fb7247e6a1a5d7e6a382f1baf95495eb830478
Vita Chumakova <me@ezhevita.dev>
2025-11-24 13:41:57 +0400

    fix: remove magsafe wupa responses

--- armsrc/iso14443a.c
+++ armsrc/iso14443a.c
@@ -1832,19 +1832,8 @@ void SimulateIso14443aTag(uint8_t tagType, uint16_t flags, uint8_t *useruid, uin
             p_response = NULL;
 
         } else if (receivedCmd[0] == ISO14443A_CMD_REQA && len == 1) { // Received a REQUEST, but in HALTED, skip
-            odd_reply = !odd_reply;
-            if (odd_reply) {
-                p_response = &responses[RESP_INDEX_ATQA];
-            }
+            p_response = &responses[RESP_INDEX_ATQA];
         } else if (receivedCmd[0] == ISO14443A_CMD_WUPA && len == 1) { // Received a WAKEUP
-            p_response = &responses[RESP_INDEX_ATQA];
-        } else if (receivedCmd[0] == MAGSAFE_CMD_WUPA_1 && len == 1) { // Received a magsafe WAKEUP
-            p_response = &responses[RESP_INDEX_ATQA];
-        } else if (receivedCmd[0] == MAGSAFE_CMD_WUPA_2 && len == 1) { // Received a magsafe WAKEUP
-            p_response = &responses[RESP_INDEX_ATQA];
-        } else if (receivedCmd[0] == MAGSAFE_CMD_WUPA_3 && len == 1) { // Received a magsafe WAKEUP
-            p_response = &responses[RESP_INDEX_ATQA];
-        } else if (receivedCmd[0] == MAGSAFE_CMD_WUPA_4 && len == 1) { // Received a magsafe WAKEUP
             p_response = &responses[RESP_INDEX_ATQA];
         } else if (receivedCmd[1] == 0x20 && receivedCmd[0] == ISO14443A_CMD_ANTICOLL_OR_SELECT && len == 2) {    // Received request for UID (cascade 1)
             p_response = &responses[RESP_INDEX_UIDC1];
